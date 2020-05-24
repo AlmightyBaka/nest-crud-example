7916b57137ef640206dac2c40635e280dff1a2e4
Almighty Baka <ivanfedulov@almightybaka.com>
2020-05-24 19:06:36 +0300

    added Mongo connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
-import { Module } from '@nestjs/common';
-import { CatController } from './entities/cats/cat.controller';
-import { CatGroupController } from './entities/catGroup/catGroup.controller';
-import { CatService } from './entities/cats/cat.service';
-import { CatGroupService } from './entities/catGroup/catGroup.service';
+import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { CatController } from './entities/cats/cat.controller'
+import { CatGroupController } from './entities/catGroup/catGroup.controller'
+import { CatService } from './entities/cats/cat.service'
+import { CatGroupService } from './entities/catGroup/catGroup.service'
 // import { GraphQLModule } from '@nestjs/graphql';
 
 
@@ -12,6 +13,15 @@ import { CatGroupService } from './entities/catGroup/catGroup.service';
     // GraphQLModule.forRoot({
     //   typeDefs: "./keystore.graphql"
     // }),
+    // TODO: move out of code
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: 'localhost',
+      port: 27017,
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    })
   ],
   controllers: [CatController, CatGroupController],
   providers: [CatService, CatGroupService],
